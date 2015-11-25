996a9f389231de06b8cd99cb47ebf2b44cca439f
Nedja995 <nedjapejasinovic@gmail.com>
2015-11-25 18:59:37 +0100

    test changed

--- npcv/test/TestManager.cpp
+++ npcv/test/TestManager.cpp
@@ -155,7 +155,7 @@ int main(int argc, char** argv)
 {
 	std::cout << "===== NPCV TestManger started=====" << std::endl;
 
-	NPCVTests testChoosed = ClassifyOcr;
+	NPCVTests testChoosed = ImageReadWrite;
 
 	std::string input;
 
